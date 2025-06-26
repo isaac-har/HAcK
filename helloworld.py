cf6d12eb14a3ac477ae11bbe0519cdcfdf64326d
Isaac Harrington <isaachar202@gmail.com>
2025-06-25 21:04:37 -0700

    Mean function added

--- helloworld.py
+++ helloworld.py
@@ -1,2 +1,8 @@
-print("Hello world!")
-print("Hello Universe")
+def mean(input):
+  sum = 0
+  for entry in input:
+    sum += entry
+  return sum / len(input)
+
+my_list = [0, 1, 2, 3, 4, 5]
+print(mean(my_list))

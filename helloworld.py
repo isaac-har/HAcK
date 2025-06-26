a3be09c1344d10a4b5dfdd6ca2fcf9f87546d211
Isaac Harrington <isaachar202@gmail.com>
2025-06-25 21:05:25 -0700

    Median function added

--- helloworld.py
+++ helloworld.py
@@ -1,2 +1,14 @@
-print("Hello world!")
-print("Hello Universe")
+def median(input):
+  list.sort(input)
+  middleIndex = len(input)//2
+
+  if(len(input) % 2 == 0):
+    medianValue = (input[middleIndex] + input[middleIndex - 1]) / 2
+  else:
+    medianValue = input[middleIndex]
+
+  return medianValue
+
+my_list = [0, 1, 2, 3, 4, 5]
+print(median(my_list))
+
